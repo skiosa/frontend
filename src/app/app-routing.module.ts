8cea9d37446b2aeeeca5a65af3e3c73b603631ba
Tim Horlacher <tim.horlacher@protonmail.com>
2022-03-21 14:58:56 +0000

    SKIOS-29 Added undesigned boilerplate (#1)
    * Added undesigned boilerplate
    
    * Added API-URL & Fixed Name
    
    Co-authored-by: Lukas <lukas@leucali.net>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
