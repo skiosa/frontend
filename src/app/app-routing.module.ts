b439d303bab830a455706ac62a47675944194410
Simon Morgenstern <inf2012@lehre.dhbw-stuttgart.de>
2022-04-13 08:58:41 +0000

    feat(article): add article module and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [
 		loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule)
 	},
 	{
+		path: 'article', 
+		loadChildren: () => import('./pages/article/article.module').then(m => m.ArticleModule)
+	},
+	{
 		path: '**',
 		redirectTo: '404'
 	}
