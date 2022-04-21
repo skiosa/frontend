7ce65f404c5ed60f4c36a639f1cdb777cd78adc1
DrShadowPLay <52300261+DrShadowPLay@users.noreply.github.com>
2022-04-21 12:38:15 +0000

    Added first Favicon + changes on Theme

---
+++ src/app/core/services/favicon.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+
+
+export class FaviconService {
+
+
+	/**
+   * @author Marcel Alex
+   * @summary Changes the favicon based on current settings
+   * @description Changes the favicon based on current settings
+   * @returns {void}
+   */
+	changeFavicon()  { 
+		const favIcon = document.getElementById('faviconTag') as HTMLLinkElement;
+		if (favIcon) {
+			const isDark = window.matchMedia('(prefers-color-scheme: dark)');
+			if (isDark.matches) {
+				favIcon.href = 'assets/favicons/Favicon_dark.svg';
+			} else {
+				favIcon.href = 'assets/favicons/Favicon_light.svg';
+			}
+		}
+	}
+  
+}
