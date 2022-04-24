589e0baec1b1e76edb6ca8bc0931b5b469ecb284
Lukas <lukas@leucali.net>
2022-04-24 13:31:19 +0200

    Format

--- src/app/pages/welcome/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome/welcome-page/welcome-page.component.ts
@@ -11,46 +11,46 @@ import { GENERAL_RECOMMENDATION_QUERY } from 'src/app/core/queries/recommendatio
 export class WelcomePageComponent implements OnInit {
 	constructor(private apollo: Apollo) { }
 
-  public recommendedArticles: Article[] = [];
-  private seed: number = Math.random();
-  private skip = 0;
-  private take = 10;
+	public recommendedArticles: Article[] = [];
+	private seed: number = Math.random();
+	private skip = 0;
+	private take = 10;
 
-  ngOnInit(): void {
-  	this.apollo
-  		.watchQuery({
-  			query: GENERAL_RECOMMENDATION_QUERY,
-  			variables: {
-  				seed: this.seed,
-  				PaginationArg: {
-  					skip: this.skip,
-  					take: this.take,
-  				},
-  			},
-  			// eslint-disable-next-line @typescript-eslint/no-explicit-any
-  		})
-  		.valueChanges.subscribe(({ data }: any) => {
-  			this.recommendedArticles = data.recommendedArticles;
-  		});
-  }
+	ngOnInit(): void {
+		this.apollo
+			.watchQuery({
+				query: GENERAL_RECOMMENDATION_QUERY,
+				variables: {
+					seed: this.seed,
+					PaginationArg: {
+						skip: this.skip,
+						take: this.take,
+					},
+				},
+				// eslint-disable-next-line @typescript-eslint/no-explicit-any
+			})
+			.valueChanges.subscribe(({ data }: any) => {
+				this.recommendedArticles = data.recommendedArticles;
+			});
+	}
 
-  /**
-   * @author Amos Gross
-   * @summary shortens text to 80 chars
-   * @description shortens a given string down and adds '...' if needed
-   * @param {string} text - string to shorten
-   * @returns {string} shortened text
-   */
-  public shortenedText(text: string): string {
-  	if (text.length <= 80) {
-  		return text;
-  	} else {
-  		let trimLen = 77;
-  		while (text.charAt(trimLen) !== ' ' || trimLen === 0) {
-  			trimLen--;
-  		}
+	/**
+	 * @author Amos Gross
+	 * @summary shortens text to 80 chars
+	 * @description shortens a given string down and adds '...' if needed
+	 * @param {string} text - string to shorten
+	 * @returns {string} shortened text
+	 */
+	public shortenedText(text: string): string {
+		if (text.length <= 80) {
+			return text;
+		} else {
+			let trimLen = 77;
+			while (text.charAt(trimLen) !== ' ' || trimLen === 0) {
+				trimLen--;
+			}
 
-  		return text.substring(0, trimLen) + '...';
-  	}
-  }
+			return text.substring(0, trimLen) + '...';
+		}
+	}
 }
