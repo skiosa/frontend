9a312b5f424381f813a15c28d8ee86db21fc8caa
DrShadowPLay <marci.alex1@gmail.com>
2022-04-26 21:05:50 +0200

    start

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,19 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
-  },
-  {
-    path: '**',
-    redirectTo: '404',
-  },
+	{
+		path: '',
+		loadChildren: () =>
+			import('./pages/welcome/welcome.module').then((m) => m.WelcomeModule),
+	},
+	{ path: '/feed-overview',
+		redirectTo:('./pages/feed-overview/feed-overview.module'),
+	},
+	{
+		path: '**',
+		redirectTo: '404',
+	},
+
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
