d5dadd1f93a47521d482ebfa9d6a8ce707b19cc6
Simon Morgenstern <inf20128@lehre.dhbw-stuttgart.de>
2022-04-26 15:37:51 +0000

    feat(feed): add bg color based on color seed

--- src/app/shared/feed/feed.component.ts
+++ src/app/shared/feed/feed.component.ts
@@ -8,8 +8,30 @@ import { Component, Input, OnInit } from '@angular/core';
 export class FeedComponent implements OnInit{
 	@Input() title = ''
 	@Input() icon = ''
+	@Input() colorSeed = 0;
+
 	iconLink = ''
 	ngOnInit(): void {
 		this.iconLink = `assets/icons/${this.icon}.svg`;
+	}
+	/**
+   * @author Amos Gross
+   * @summary fetches color for article
+   * @description preliminary solution for generating colored articles
+   * @returns {string} css class for color
+   */
+	getColor(): string {
+		switch (this.colorSeed % 4) {
+		case 0:
+			return 'color-a';
+		case 1:
+			return 'color-b';
+		case 2:
+			return 'color-c';
+		case 3:
+			return 'color-d';
+		default:
+			return 'color-a';
+		}
 	}
 }
