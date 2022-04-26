d2de1bd91784f04f6cc3b116cd06840a4ff49b95
grossamos <email@amosgross.com>
2022-04-26 18:11:19 +0000

    fixed unit tests

--- src/app/pages/welcome/welcome-page/welcome-page.component.spec.ts
+++ src/app/pages/welcome/welcome-page/welcome-page.component.spec.ts
@@ -2,17 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { KeycloakAngularModule } from 'keycloak-angular';
 import { WelcomePageComponent } from './welcome-page.component';
 import { HttpClientModule } from '@angular/common/http';
-import { RecomendationService } from 'src/app/core/services/recomendation.service';
 
 describe('WelcomePageComponent', () => {
   let comp: WelcomePageComponent;
   beforeEach(async () => {
     TestBed.configureTestingModule({
       declarations: [WelcomePageComponent],
-      providers: [
-        WelcomePageComponent,
-        { provide: RecomendationService, useClass: MockRecomendationService },
-      ],
+      providers: [WelcomePageComponent],
       imports: [KeycloakAngularModule, HttpClientModule],
     }).compileComponents();
     comp = TestBed.inject(WelcomePageComponent);
@@ -23,23 +19,4 @@ describe('WelcomePageComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it('should not trim short strings', () => {
-    const text = 'abc';
-    const res = comp.shortenedText(text);
-    expect(res).toEqual(text);
-  });
-
-  it('should trim long strings', () => {
-    const text =
-      'abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde abcdeabcde';
-    const expected =
-      'abcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcdeabcde...';
-    const res = comp.shortenedText(text);
-    expect(res).toEqual(expected);
-  });
 });
-
-class MockRecomendationService {
-  getGeneralArticles = [];
-}
