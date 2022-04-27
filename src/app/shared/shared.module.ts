b52db7b8e376ef5631401a09dac789c52f2dba5d
Simon Morgenstern <inf20128@lehre.dhbw-stuttgart.de>
2022-04-27 09:58:29 +0000

    feat: add button

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { ArticleComponent } from './article/article.component';
 import { FeedComponent } from './feed/feed.component';
 import { SubscriptionFeedComponent } from './subscription-feed/subscription-feed.component';
 import { NavButtonComponent } from './nav-button/nav-button.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
-  declarations: [CardComponent, ArticleComponent, FeedComponent, SubscriptionFeedComponent, NavButtonComponent],
+  declarations: [CardComponent, ArticleComponent, FeedComponent, SubscriptionFeedComponent, NavButtonComponent, ButtonComponent],
   imports: [CommonModule],
-  exports: [CardComponent, ArticleComponent, FeedComponent, SubscriptionFeedComponent, NavButtonComponent],
+  exports: [CardComponent, ArticleComponent, FeedComponent, SubscriptionFeedComponent, NavButtonComponent, ButtonComponent],
 })
 export class SharedModule {}
