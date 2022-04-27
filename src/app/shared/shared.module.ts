a399d12cc4ef73607ce245daf32ef7e4fe6869a1
jeppard <35837126+jeppard@users.noreply.github.com>
2022-04-27 10:38:32 +0000

    Added Icons

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { ArticleComponent } from './article/article.component';
+import { IconButtonComponent } from './icon-button/icon-button.component';
 
 @NgModule({
-	declarations: [CardComponent, ArticleComponent],
+	declarations: [CardComponent, ArticleComponent, IconButtonComponent],
 	imports: [CommonModule],
 	exports: [CardComponent, ArticleComponent],
 })
--- src/app/util/test.ts
+++
@@ -1,7 +0,0 @@
-import { Article } from 'skiosa-orm';
-import { PartialExcept, RecursivePartial } from './types';
-
-type A = RecursivePartial<Article>
-type B = A['feed'];
-type C = PartialExcept<Article, 'title' | 'content'>;
-type D = C['feed']
