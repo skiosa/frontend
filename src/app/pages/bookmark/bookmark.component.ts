390fdc0b68529b04b8190b03e10c02c574d3bff7
Lukas <lukas@leucali.net>
2022-05-08 14:50:44 +0200

    Removed Import

--- src/app/pages/bookmark/bookmark.component.ts
+++ src/app/pages/bookmark/bookmark.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { Apollo } from 'apollo-angular';
 import { BOOKMARKS_QUERY, BOOKMARKS_QUERY_RESPONSE } from 'src/app/core/queries/bookmarks';
 import { getColorSeedFromArticle } from 'src/app/util/randomColor';
