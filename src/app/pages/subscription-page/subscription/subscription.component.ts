a37a345fecaef6e1966a101991274af5f2e06d05
Lukas <32310269+LukasLJL@users.noreply.github.com>
2022-05-10 11:07:19 +0200

    Skios 176 subscription page not updated after a new feed is subscribed (#30)
    * Fixed Subscription reload
    
    * Update
    
    * New Update WoW
    
    * Connected Links
    
    * Removed Imports
    
    * Added sonar supress
    
    Co-authored-by: grossamos <email@amosgross.com>

--- src/app/pages/subscription-page/subscription/subscription.component.ts
+++ src/app/pages/subscription-page/subscription/subscription.component.ts
@@ -14,23 +14,10 @@ export class SubscriptionComponent implements OnInit {
 	private skip = 0;
 	private take = 10;
 
-	constructor(private apollo: Apollo) {}
+	constructor(private apollo: Apollo) { }
 
 	ngOnInit(): void {
-		this.apollo
-			.watchQuery<SUBSCRIPTION_QUERY_RESPONSE>({
-				query: SUBSCRIPTION_QUERY,
-				variables: {
-					PaginationArg: {
-						skip: this.skip,
-						take: this.take,
-					},
-					desc: true,
-				},
-			})
-			.valueChanges.subscribe(({ data }) => {
-				this.subscriptions = data.subscriptions;
-			});
+		this.fetchSubscriptions();
 	}
 
 	/**
@@ -58,6 +45,27 @@ export class SubscriptionComponent implements OnInit {
 			this.visibleSubscriptions.delete(feedId);
 		};
 	};
+
+	/**
+	 * @author Amos Gross
+	 * @summary fetches subscriptions
+	 * @description loads subscription data from graphql
+	 */
+	fetchSubscriptions = () => {
+		this.apollo.watchQuery<SUBSCRIPTION_QUERY_RESPONSE>({
+			query: SUBSCRIPTION_QUERY,
+			fetchPolicy: 'network-only',
+			variables: {
+				PaginationArg: {
+					skip: this.skip,
+					take: this.take,
+				},
+				desc: true,
+			},
+		}).valueChanges.subscribe(({ data }) => {
+			this.subscriptions = data.subscriptions;
+		});
+	}
 
 	getColorSeed(article: SUBSCRIPTION_QUERY_RESPONSE['subscriptions'][0]['articles'][0]): number {
 		return getColorSeedFromArticle(article);
