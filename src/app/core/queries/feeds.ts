ed4572c69b0895f4f66a1274f442c18652a1467f
jeppard <35837126+jeppard@users.noreply.github.com>
2022-05-10 08:38:05 +0000

    Sidebar Links

--- src/app/core/queries/feeds.ts
+++ src/app/core/queries/feeds.ts
@@ -24,7 +24,7 @@ export type GENERAL_FEED_QUERY_RESPONCE = {
  * @description query to a feed and its articles
  * @param {PaginationArg} paginationArg -  Pagination to limit/paginate the articles of an feed
  * @param {number} feedId - feedId to fetch the selected feed
- * @param {boolean} desc - Descending order of articles by the publishedAt Date 
+ * @param {boolean} desc - Descending order of articles by the publishedAt Date
  * @returns {GENERAL_FEED_QUERY_RESPONCE} - Feed which contains the articles of the feed and the feed itself
  */
 export const GENERAL_FEED_QUERY = gql<GENERAL_FEED_QUERY_RESPONCE, { feedId: number; desc: boolean }>`
--- src/app/core/queries/subscription.ts
+++ src/app/core/queries/subscription.ts
@@ -26,7 +26,7 @@ export type SUBSCRIPTION_QUERY_RESPONSE = {
  * @summary query for user subscriptions
  * @description query to fetch all subscriptions of a user
  * @param {PaginationArg} paginationArg -  Pagination to limit/paginate the articles of an feed-subscription
- * @param {boolean} desc - Descending order of articles by the publishedAt Date 
+ * @param {boolean} desc - Descending order of articles by the publishedAt Date
  * @returns {SUBSCRIPTION_QUERY_RESPONSE} - Subscriptions of the user which contains the subscribed feeds and their articles
  */
 export const SUBSCRIPTION_QUERY = gql<SUBSCRIPTION_QUERY_RESPONSE, { PaginationArg: PaginationArg; desc: boolean }>`
