58ebd95ed37d39632b0a12ba18478376b8bf3758
Tim Horlacher <tim.horlacher@protonmail.com>
2022-05-10 09:44:21 +0000

    Fixed tests

--- src/app/shared/article/article.component.spec.ts
+++ src/app/shared/article/article.component.spec.ts
@@ -1,5 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ApolloModule } from 'apollo-angular';
+import { KeycloakAngularModule } from 'keycloak-angular';
 import { ArticleComponent } from './article.component';
 
 describe('ArticleComponent', () => {
@@ -9,6 +11,7 @@ describe('ArticleComponent', () => {
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
 			declarations: [ArticleComponent],
+			imports: [ApolloModule, HttpClientTestingModule, KeycloakAngularModule]
 		}).compileComponents();
 	});
 
